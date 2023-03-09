7abc9b0273728a9a9d773b3c46e2e4f07d78f816
Sergey M <12753171+s3rgeym@users.noreply.github.com>
2023-03-09 23:03:44 +0000

    Вызов произвольного метода API

---
+++ hh_applicant_tool/operations/api_call.py
@@ -0,0 +1,37 @@
+# Этот модуль можно использовать как образец для других
+import argparse
+import logging
+
+from ..api import ApiClient
+from ..main import BaseOperation
+from ..main import Namespace as BaseNamespace
+from ..utils import dumps
+
+logger = logging.getLogger(__package__)
+
+
+class Namespace(BaseNamespace):
+    method: str
+    endpoint: str
+    params: list[str]
+
+
+class Operation(BaseOperation):
+    """Вызвать произвольный метод API <https://github.com/hhru/api>."""
+
+    def setup_parser(self, parser: argparse.ArgumentParser) -> None:
+        parser.add_argument("endpoint")
+        parser.add_argument(
+            "param", nargs="*", help="PARAM=VALUE", dest="params", default=[]
+        )
+        parser.add_argument("-m", "--method", "--meth", help="HTTP Метод")
+
+    def run(self, args: Namespace) -> None:
+        assert args.config["token"]
+        api = ApiClient(
+            access_token=args.config["token"]["access_token"],
+            user_agent=args.config["user_agent"],
+        )
+        params = dict(para.split('=', 1) for para in args.params)
+        result = api.request(args.method, args.endpoint, params=params)
+        print(dumps(result))
