bc4934d1369b19a703d0751373cf44eacf3df087
Sergey M <12753171+s3rgeym@users.noreply.github.com>
2023-03-10 05:53:35 +0300

    список сайтов работодателей

---
+++ get-negotiations-employer-site-urls.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+# ./get-negotiations-employer-site-urls.sh | grep -v '^$' | sort | uniq > sites.txt
+cd "$(dirname "$0")"
+. .venv/bin/activate
+
+page=0
+per_page=100
+while true; do
+  output=$(python -m hh_applicant_tool -vv call-api /negotiations "page=$page" "per_page=$per_page")
+  urls=($(jq -r '.items[].vacancy.employer.url' <<< "$output"))
+  for url in "${urls[@]}"; do
+    curl -s "$url" | jq -r .site_url
+  done
+  pages=$(jq .pages <<< "$output")
+  ((++page))
+  if [ $page -ge $pages ]; then
+    break
+  fi
+done
