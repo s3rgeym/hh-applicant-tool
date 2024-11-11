d2896680eef0837d6db53d4c6eddfe0fae2dd565
Sergey M <12753171+s3rgeym@users.noreply.github.com>
2024-11-11 06:25:56 +0300

    небольшие исправления

--- hh_applicant_tool/telemetry_client.py
+++ hh_applicant_tool/telemetry_client.py
@@ -8,15 +8,20 @@ from functools import cache
 
 class TelemetryError(Exception):
     """Исключение, возникающее при ошибках в работе TelemetryClient."""
+
     pass
 
 
 class TelemetryClient:
     """Клиент для отправки телеметрии на сервер."""
 
-    server_address = 'http://127.0.0.1:12345'
-  
-    def __init__(self, server_address: Optional[str] = None, session: Optional[requests.Session] = None) -> None:
+    server_address = "http://127.0.0.1:12345"
+
+    def __init__(
+        self,
+        server_address: Optional[str] = None,
+        session: Optional[requests.Session] = None,
+    ) -> None:
         """
         Инициализация клиента.
 
@@ -24,9 +29,13 @@ class TelemetryClient:
         :param session: Сессия для повторного использования соединения.
         """
         self.session = session or requests.Session()
-        self.server_address = os.getenv('TELEMETRY_SERVER', server_address or self.server_address)
-      
-    def send_telemetry(self, endpoint: str, data: Dict[str, Any]) -> Dict[str, Any]:
+        self.server_address = os.getenv(
+            "TELEMETRY_SERVER", server_address or self.server_address
+        )
+
+    def send_telemetry(
+        self, endpoint: str, data: Dict[str, Any]
+    ) -> Dict[str, Any]:
         """
         Отправка телеметрии на сервер.
 
@@ -36,15 +45,16 @@ class TelemetryClient:
         :raises TelemetryError: Если произошла ошибка при отправке или декодировании JSON.
         """
         url = urljoin(self.server_address, endpoint)
-        headers = {
-            'Content-Type': 'application/json'
-        }
+        headers = {"Content-Type": "application/json"}
 
         try:
             response = self.session.post(url, headers=headers, json=data)
             response.raise_for_status()
             return response.json()
-        except (requests.exceptions.RequestException, json.JSONDecodeError) as ex:
+        except (
+            requests.exceptions.RequestException,
+            json.JSONDecodeError,
+        ) as ex:
             raise TelemetryError(str(ex)) from ex
 
 
