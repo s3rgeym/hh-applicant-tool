eb1f301dcc0fedd6dc339420f1e0d44ec85ccf11
Sergey M <12753171+s3rgeym@users.noreply.github.com>
2024-11-15 00:55:42 +0300

    fix: отправка телеметрии осуществлялась без использования прокси

--- hh_applicant_tool/telemetry_client.py
+++ hh_applicant_tool/telemetry_client.py
@@ -5,6 +5,7 @@ import requests
 from typing import Optional, Dict, Any
 import logging
 import base64
+from functools import partialmethod
 
 logger = logging.getLogger(__package__)
 
@@ -25,33 +26,36 @@ class TelemetryClient:
     def __init__(
         self,
         server_address: Optional[str] = None,
+        *,
         session: Optional[requests.Session] = None,
+        user_agent: str = "Mozilla/5.0 (HHApplicantTelemetry/1.0)",
+        proxies: dict | None = None,
     ) -> None:
-        """
-        Инициализация клиента.
-
-        :param server_address: Адрес сервера для отправки телеметрии.
-        :param session: Сессия для повторного использования соединения.
-        """
-        self.session = session or requests.Session()
         self.server_address = os.getenv(
             "TELEMETRY_SERVER", server_address or self.server_address
         )
+        self.session = session or requests.Session()
+        self.user_agent = user_agent
+        self.proxies = proxies
 
-    def send_telemetry(
-        self, endpoint: str, data: Dict[str, Any]
+    def request(
+        self,
+        method: str,
+        endpoint: str,
+        data: Dict[str, Any] | None = None,
     ) -> Dict[str, Any]:
-        """
-        Отправка телеметрии на сервер.
-
-        :param endpoint: Конечная точка на сервере.
-        :param data: Данные для отправки.
-        :return: Ответ сервера в формате JSON.
-        :raises TelemetryError: Если произошла ошибка при отправке или декодировании JSON.
-        """
+        method = method.upper()
         url = urljoin(self.server_address, endpoint)
+        has_body = method in ["POST", "PUT", "PATCH"]
         try:
-            response = self.session.post(url, json=data)
+            response = self.session.request(
+                method,
+                url,
+                headers={"User-Agent": self.user_agent},
+                proxies=self.proxies,
+                params=data if not has_body else None,
+                json=data if has_body else None,
+            )
             # response.raise_for_status()
             result = response.json()
             if "error" in result:
@@ -63,3 +67,5 @@ class TelemetryClient:
             json.JSONDecodeError,
         ) as ex:
             raise TelemetryError(str(ex)) from ex
+
+    send_telemetry = partialmethod(request, "POST")
