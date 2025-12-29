9bbc5d508399e0e88c3bf765249a2653341ac6b4
Sergey M <12753171+s3rgeym@users.noreply.github.com>
2025-12-29 23:02:43 +0300

    Rename 'no_magic' to 'magic' in apply_similar.py
    В питоне 3.14 нельзя --no использовать в подобных флагах???

--- hh_applicant_tool/operations/apply_similar.py
+++ hh_applicant_tool/operations/apply_similar.py
@@ -60,7 +60,7 @@ class Namespace(BaseNamespace):
     right_lng: float | None
     sort_point_lat: float | None
     sort_point_lng: float | None
-    no_magic: bool
+    magic: bool
     premium: bool
 
 
@@ -191,10 +191,10 @@ class Operation(BaseOperation, GetResumeIdMixin):
             help="Координата lng для сортировки по расстоянию",
         )
         parser.add_argument(
-            "--no-magic",
-            default=False,
+            "--magic",
+            default=True,
             action=argparse.BooleanOptionalAction,
-            help="Отключить авторазбор текста запроса",
+            help="Включить авторазбор текста запроса",
         )
         parser.add_argument(
             "--premium",
@@ -291,7 +291,7 @@ class Operation(BaseOperation, GetResumeIdMixin):
         self.sort_point_lng = args.sort_point_lng
         self.clusters = args.clusters
         # self.describe_arguments = args.describe_arguments
-        self.no_magic = args.no_magic
+        self.no_magic = not args.magic
         self.premium = args.premium
         self._apply_similar()
 
