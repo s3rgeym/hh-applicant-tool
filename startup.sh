1768808769a7047b31987f8ff175171611d3c770
Sergey M <12753171+s3rgeym@users.noreply.github.com>
2026-01-13 12:39:12 +0300

    рассылка откликов с разных акков. исправил баг, когда при при ошибке терялся токен доступа, если он обновился до этого

--- startup.sh
+++ startup.sh
@@ -3,9 +3,7 @@ echo "[$(date)] Running startup tasks..."
 
 # Выполняем цепочку
 /usr/local/bin/python -m hh_applicant_tool -c /app/config refresh-token
-sleep 10
 /usr/local/bin/python -m hh_applicant_tool -c /app/config update-resumes
-sleep 10
 /usr/local/bin/python -m hh_applicant_tool -c /app/config apply-similar
 
 echo "[$(date)] Startup tasks finished."
