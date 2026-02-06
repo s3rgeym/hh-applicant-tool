0f841773c2fc38621961fb3abe3428e115ae4a87
Sergey M <12753171+s3rgeym@users.noreply.github.com>
2026-02-07 02:37:16 +0300

    test session

---
+++ src/hh_applicant_tool/operations/test_session.py
@@ -0,0 +1,37 @@
+# Этот модуль можно использовать как образец для других
+from __future__ import annotations
+
+import argparse
+import logging
+from typing import TYPE_CHECKING
+
+from hh_applicant_tool.api.errors import ApiError
+
+from ..main import BaseNamespace, BaseOperation
+
+if TYPE_CHECKING:
+    from ..main import HHApplicantTool
+
+
+logger = logging.getLogger(__package__)
+
+
+class Namespace(BaseNamespace):
+    pass
+
+
+class Operation(BaseOperation):
+    """Проверка браузерной сессии, полученной при авторизации"""
+
+    __aliases__: list[str] = []
+
+    def setup_parser(self, parser: argparse.ArgumentParser) -> None:
+        # parser
+        ...
+
+    def run(self, tool: HHApplicantTool) -> None:
+        r = tool.session.get("https://hh.ru/")
+        for header, value in r.headers.items():
+            print(f"{header}:\t{value}")
+        print()
+        print(r.text)
